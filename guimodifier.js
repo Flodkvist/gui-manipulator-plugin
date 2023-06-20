6ad392354d26b36390f904b48b5f03daf39b2668
Flodkvist <31692548+Flodkvist@users.noreply.github.com>
2023-06-20 13:50:45 +0200

    Squashed commit of the following:
    commit b91b56a7c9de2ab7ddb069d5a680eb0a85c8637f
    Author: Flodkvist <31692548+Flodkvist@users.noreply.github.com>
    Date:   Tue Jun 20 13:46:34 2023 +0200
    
        Button moved to mapmenu
    
        Moved button to mapmenu and some cleanup.
    
    commit bb11bccc8364fbbf5d2d2dc10861266616320299
    Author: Flodkvist <31692548+Flodkvist@users.noreply.github.com>
    Date:   Mon Jun 19 16:06:54 2023 +0200
    
        Altered manipulation strategy
    
        Functional strategy for dynamic alteration of choosen css through style sheet rules instead of element styles.

--- guimodifier.js
+++ guimodifier.js
@@ -11,100 +11,93 @@ const Guimodifier = function Guimodifier(options = {}) {
   let target;
   let isCompact;
 
-  const ySmallEl = document.querySelectorAll(".o-ui .padding-y-small");
-  const topSmallEl = document.querySelectorAll(".o-ui .padding-top-small");
-  const padYsmallOriginal = "0.5rem";
-  const padYsmallCompact = "0.1rem";
+  let mapMenu;
 
-  const liItems = document.querySelectorAll(".o-ui .list > li, .o-ui .list > .item");
-  const padListItemsOriginal = "0.25rem";
-  const padListItemsCompact = "0.1rem";
-
-  const liFirstItems = document.querySelectorAll(".list > li:first-child, .o-ui .list > .item:first-child");
-  const liLastItems = document.querySelectorAll(".o-ui .list > li:last-child, .o-ui .list > .item:last-child");
-  const padListFirstLastOriginal = "0.5rem";
-  const padListFirstLastCompact = "0.25rem";
+  let styleSheet;
+  let elementRules;
 
   function setMode(mode){
     if (mode == "compact"){
-      ySmallEl.forEach(el => {
-        el.style.paddingTop = padYsmallCompact;
-        el.style.paddingBottom = padYsmallCompact;
-      });
-      topSmallEl.forEach(el => {
-        el.style.paddingTop = padYsmallCompact;
-      });
-      liItems.forEach(el => {
-        el.style.paddingTop = padListItemsCompact;
-        el.style.paddingBottom = padListItemsCompact;
-      });
-      liFirstItems.forEach(el => {
-        el.style.paddingTop = padListFirstLastCompact;
-      });
-      liLastItems.forEach(el => {
-        el.style.paddingBottom = padListFirstLastCompact;
-      });
+      for(let i = 0; i < styleSheet.cssRules.length; i++) {
+        if(styleSheet.cssRules[i].selectorText === '.o-ui .padding-y-small') {
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.1rem');
+          styleSheet.cssRules[i].style.setProperty('padding-bottom','0.1rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText === '.o-ui .padding-top-small'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.1rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li, .o-ui .list > .item'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.05rem');
+          styleSheet.cssRules[i].style.setProperty('padding-bottom','0.05rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li:first-child, .o-ui .list > .item:first-child'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.1rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li:last-child, .o-ui .list > .item:last-child'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.1rem');
+        }
+      }
       isCompact = true;
     } else {
-      ySmallEl.forEach(el => {
-        el.style.paddingTop = padYsmallOriginal;
-        el.style.paddingBottom = padYsmallOriginal;
-      });
-      topSmallEl.forEach(el => {
-        el.style.paddingTop = padYsmallOriginal;
-      });
-      liItems.forEach(el => {
-        el.style.paddingTop = padListItemsOriginal;
-        el.style.paddingBottom = padListItemsOriginal;
-      });
-      liFirstItems.forEach(el => {
-        el.style.paddingTop = padListFirstLastOriginal;
-      });
-      liLastItems.forEach(el => {
-        el.style.paddingBottom = padListFirstLastOriginal;
-      });
+      for(let i = 0; i < styleSheet.cssRules.length; i++) {
+        if(styleSheet.cssRules[i].selectorText === '.o-ui .padding-y-small') {
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.5rem');
+          styleSheet.cssRules[i].style.setProperty('padding-bottom','0.5rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText === '.o-ui .padding-top-small'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.5rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li, .o-ui .list > .item'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.25rem');
+          styleSheet.cssRules[i].style.setProperty('padding-bottom','0.25rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li:first-child, .o-ui .list > .item:first-child'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.5rem');
+        }
+        else if (styleSheet.cssRules[i].selectorText ==='.o-ui .list > li:last-child, .o-ui .list > .item:last-child'){
+          styleSheet.cssRules[i].style.setProperty('padding-top','0.5rem');
+        }
+      }
       isCompact = false;
     }
   }
 
   return Origo.ui.Component({
     name: 'guimodifier',
-    onInit() {
-    /*  document.querySelectorAll("span.grow.padding-x-small").forEach(el => {
-        el.style.fontWeight = "600";
-      }); */
+    onAdd(evt) {
+      viewer = evt.target;
+      mapMenu = viewer.getControlByName('mapmenu');
+      styleSheet  = document.styleSheets[0];
 
-      guiToggler = Origo.ui.Button({
-        cls: 'padding-small icon-smaller round light box-shadow',
+      guiToggler = mapMenu.MenuItem({
         click() {
           if (isCompact) {
             setMode("original");
           } else {
             setMode("compact");
           }
+          mapMenu.close();
         },
-        icon,
-        tooltipText: 'Växla mellan vanliga eller kompakta menyer',
-        tooltipPlacement: 'east'
+        icon: icon,
+        title: 'Växla vy'
       });
+
       if (initialMode == "compact"){
         setMode("compact");
       } else {
         isCompact = false;
       }
-    },
-    onAdd(evt) {
-      viewer = evt.target;
-      isCompact = false;
-      if (!target) target = `${viewer.getMain().getNavigation().getId()}`;
-      this.addComponents([guiToggler]);
+
+      this.addComponent(guiToggler);
+      this.on('render', this.onRender);
       this.render();
+      this.dispatch('render');
+    },
+    onInit() {
+
     },
     render() {
-      const htmlString = guiToggler.render();
-      const el = Origo.ui.dom.html(htmlString);
-      document.getElementById(target).appendChild(el);
-      this.dispatch('render');
+      mapMenu.appendMenuItem(guiToggler);
     }
   });
 };
