54423c58e1d28b950faaac53283923034dbed2e4
Flodkvist <31692548+Flodkvist@users.noreply.github.com>
2023-10-11 15:44:33 +0200

    Buttonfixes.
    Fix for some legendbuttons.

--- guimodifier.js
+++ guimodifier.js
@@ -13,7 +13,7 @@ const Guimodifier = function Guimodifier(options = {}) {
   let styleSheet;
   let elementRules;
 
-  function setMode(mode){
+  function setMode(mode){    //-- minifiying properies --
     if (mode == "compact"){
       for(let i = 0; i < styleSheet.cssRules.length; i++) {
         if(styleSheet.cssRules[i].selectorText === '.o-ui .padding-y-small') {
@@ -38,19 +38,17 @@ const Guimodifier = function Guimodifier(options = {}) {
           styleSheet.cssRules[i].style.setProperty('width','1rem');
         }
       }
-      const cssRule = '.grey-lightest.round.compact.icon-small.relative.no-shrink.light.o-tooltip {' +
+      const cssRule = '.grey-lightest.round.compact.icon-small.relative.no-shrink.light {' +
         'height: calc(1rem + 2px) !important;' +
         'width: calc(1rem + 2px) !important;' +
         '}' ;
       styleSheet.insertRule(cssRule, styleSheet.cssRules.length);
 
-      //-- Shrinking buttons lead to unwanted shrinkage, this fixes legend button.
-
+      //-- legend button -- Unwanted Shrinkage, original size set to element.style
       const allElements = document.getElementsByTagName('*');
       for (let i = 0; i < allElements.length; i++) {
         const element = allElements[i];
         const xlinkHref = element.getAttribute('xlink:href');
-
         if (xlinkHref === '#ic_layers_24px') {
           const parentElement = element.parentNode;
           const parentStep2element=parentElement.parentNode
@@ -61,9 +59,36 @@ const Guimodifier = function Guimodifier(options = {}) {
           break;
         }
       }
+
+      // -- background map buttons -- Unwanted Shrinkage, original size set to element.style
+      const bgButtons = document.querySelectorAll('.round.smallest.border.icon-small.icon-bg');
+      bgButtons.forEach(function(element) {
+        const bgIcons = element.querySelectorAll('.icon');
+        bgIcons.forEach(function(bgIcon) {
+          bgIcon.style.height = '1.5rem';
+          bgIcon.style.width = '1.5rem';
+          const bgImg = bgIcon.querySelector('img');
+          bgImg.style.height = '1.5rem';
+          bgImg.style.width = '1.5rem';
+        });
+      });
+
+      // -- turnofflayercontrol -- Unwanted Shrinkage, original size set to element.style
+      const layersOffBtn = document.querySelectorAll('.round.compact.icon-small.margin-x-smaller');
+      layersOffBtn.forEach(function(element) {
+        const layersOffIcons = element.querySelectorAll('.icon');
+        layersOffIcons.forEach(function(layersOffIcon) {
+          layersOffIcon.style.height = '1.5rem';
+          layersOffIcon.style.width = '1.5rem';
+          const layersOffSvg = layersOffIcon.querySelector('svg');
+          layersOffSvg.style.height = '1.5rem';
+          layersOffSvg.style.width = '1.5rem';
+        });
+      });
+
       isCompact = true;
-      //-- The following is/are/should be the original origo css properties --
-    } else {
+
+    } else {    //-- The following is/are/should be the original origo css properties --
       for(let i = 0; i < styleSheet.cssRules.length; i++) {
         if(styleSheet.cssRules[i].selectorText === '.o-ui .padding-y-small') {
           styleSheet.cssRules[i].style.setProperty('padding-top','0.5rem');
@@ -86,7 +111,7 @@ const Guimodifier = function Guimodifier(options = {}) {
           styleSheet.cssRules[i].style.setProperty('height','1.5rem');
           styleSheet.cssRules[i].style.setProperty('width','1.5rem');
         }
-        else if (styleSheet.cssRules[i].selectorText ==='.grey-lightest.round.compact.icon-small.relative.no-shrink.light.o-tooltip'){
+        else if (styleSheet.cssRules[i].selectorText ==='.grey-lightest.round.compact.icon-small.relative.no-shrink.light'){
           styleSheet.deleteRule(i);
         }
       }
